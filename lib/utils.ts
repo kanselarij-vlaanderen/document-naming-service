d1f2a2676ed4c10295b18dd462aafdc3471c7b0a
Sven Valenberghs <sven.valenberghs@faros.be>
2025-09-18 12:38:35 +0200

    cleanup unused methods and constants

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,3 @@ export function getErrorMessage(maybeError: unknown) {
   if (maybeError instanceof Error) return maybeError.message;
   else return String(maybeError);
 }
-
-export function dasherize(sentence: string): string {
-  return sentence.trim().replace(/\s/g, "-");
-}
